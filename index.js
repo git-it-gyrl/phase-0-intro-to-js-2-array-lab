f7552d8e34db17bf1dcec3dfe1acc429599fee57
celeste.tech@icloud.com <celeste.tech@icloud.com>
2021-03-30 12:57:58 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+let cats = ["Milo", "Otis","Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+ function appendCat(name) {
+    return [...cats, "Broom"];
+}
+
+function prependCat(name) {
+        return["Arnold", ...cats];
+}
+
+function removeLastCat(name) {
+        return [...cats.slice(0, 2)];
+}
+
+function removeFirstCat(name) {
+return cats.slice(1, 3);
+}
